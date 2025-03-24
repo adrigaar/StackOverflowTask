107e244bccc218d0317ba3aec770c92346cb46b5
adrigaar <adrigaar@googlemail.com>
2025-03-24 09:04:06 +0000

    test started, first red flag

---
+++ StackOverflow Task/Models/User.swift
@@ -0,0 +1,12 @@
+//
+//  User.swift
+//  StackOverflow Task
+//
+//  Created by Thomas on 24/03/2025.
+//
+
+import Foundation
+
+struct User {
+    
+}
---
+++ StackOverflow TaskTests/UserTests.swift
@@ -0,0 +1,23 @@
+//
+//  UserTests.swift
+//  StackOverflow TaskTests
+//
+//  Created by Thomas on 24/03/2025.
+//
+
+import XCTest
+@testable import StackOverflow_Task
+
+final class UserTests: XCTestCase {
+    
+    var sut: User!
+    
+    override func setUp() {
+        sut = User()
+    }
+    
+    func user_containsName() {
+        XCTAssertNotNil(sut.name)
+    }
+    
+}

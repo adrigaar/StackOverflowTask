af069f982e8774c6f77edbc4b37e0085f0c0a28a
adrigaar <adrigaar@googlemail.com>
2025-03-24 08:57:23 +0000

    initial UI setup up to first Test 'red light' phase

--- StackOverflow Task/ViewController.swift
+++ StackOverflow Task/ViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userTableView: UITableView!
+    
+    var users: [Users]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
